8bdc0620b423ff186f5da9c85854332436f225d5
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-12 00:57:52 +0800

    Initialized and closely done

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿bool CheckIfEven(int a)
+{
+    if (a % 2 == 0)
+        return true;
+    else
+        return false;    
+}
+
+int a = 7;
+if (CheckIfEven(a))
+    Console.WriteLine(a + "is an even number");
+else
+    Console.WriteLine(a + "is an odd number");
